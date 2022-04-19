d4cdb9f57d037e64ef9dadf53665719a42865056
AnasAbuHamideh <anas.abuhamida@gmail.com>
2022-04-19 23:48:49 +0300

    create class vehicle and class traffic light

---
+++ ai_traffic_system/trafic_light.py
@@ -0,0 +1,16 @@
+class TrafficLight:
+     
+    def __init__(self,red,yellow,green):
+        self.red = red
+        self.yellow = yellow
+        self.green = green
+    
+  
+t1s = TrafficLight() # straight traffic light - 1
+t1t = TrafficLight() # turn traffic light - 1
+t2s = TrafficLight() # straight traffic light - 2
+t2t = TrafficLight() # turn traffic light - 2
+t3t = TrafficLight() # turn traffic light - 3
+
+
+
